e23e869cee226c637704efb25326de396fded9c3
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-24 04:08:55 +0300

    Сделано фотографирование и сохранение фотографий

--- App.js
+++ App.js
@@ -8,10 +8,36 @@ export default function App() {
 
   const [image, setImage] = useState(null);
 
-  const photo = async () => {
+  // Запрос прав
+  const askPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
+    if (status !== 'granted') {
+      Alert.alert('Недостаточно прав. Разрешите доступ к камере и фотографиям.');
+      return false;
+    }
+    return true;
   }
 
+  // Фотографирование: запрашиваем права и открываем камеру
+  const photo = async () => {
+    const hasPermissions = await askPermissions();
+    if (!hasPermissions) {
+      return;
+    }
+    const img = await ImagePicker.launchCameraAsync({
+      quality: 0.7,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+    setImage(img.uri);
+  }
+
+  // Сохранение картинки в библиотеке
   const save = async () => {
+    await MediaLibrary.saveToLibraryAsync(image).catch((error)=> {
+      Alert.alert('Ошибка при сохранении фото:\n'+error)
+    });
+    setImage(null);
   }
 
   return (
@@ -20,7 +46,7 @@ export default function App() {
       {
         image &&
         <View>
-          <Image style={styles.image} />
+          <Image style={styles.image} source={{uri: image}} />
           <Button title="Сохранить" onPress={save}/>
         </View>
       }
